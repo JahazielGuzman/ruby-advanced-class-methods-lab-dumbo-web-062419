3e54217f78ed356f7ba0a7c310f16184832dc9cc
Jahaziel Guzman <jahaziel.guzman93@gmail.com>
2019-06-26 17:15:41 -0400

    finished advanced class methods

--- lib/song.rb
+++ lib/song.rb
@@ -2,12 +2,75 @@ class Song
   attr_accessor :name, :artist_name
   @@all = []
 
+  def initialize(name = nil, artist = nil)
+  	@name = name
+  	@artist_name = artist
+  end
+
   def self.all
     @@all
   end
 
   def save
     self.class.all << self
+  end
+
+  def self.create(name = nil, artist_name = nil)
+  	song = self.new
+  	song.name = name
+  	song.artist_name = artist_name
+  	song.save
+  	song
+  end
+
+  def self.new_by_name(name)
+  	song = self.new
+  	song.name = name
+  	song
+  end
+
+  def self.create_by_name(name)
+  	song = self.create
+  	song.name = name
+  	song
+  end
+
+  def self.find_by_name(name)
+  	self.all.find do |song|
+  		song.name == name
+  	end
+  end
+
+  def self.find_or_create_by_name(name)
+  	found_song = self.find_by_name(name)
+  	if found_song == nil
+  		self.create_by_name(name)
+  	else
+  		found_song
+  	end
+  end
+
+  def self.alphabetical
+  	self.all.sort_by do |song|
+  		song.name
+  	end
+  end
+
+  def self.new_from_filename(filename)
+  	artist, songname = filename.split(' - ')
+  	songname = songname.split('.')[0]
+  	song = self.new(songname, artist)
+  	song
+  end
+
+  def self.create_from_filename(filename)
+  	artist, songname = filename.split(' - ')
+  	songname = songname.split('.')[0]
+  	song = self.create(songname, artist)
+  end
+
+  def self.destroy_all
+  	self.all.clear
   end
 
 end
